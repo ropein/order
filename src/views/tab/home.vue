13a85e8cbfc617d924fe9391b4d81304e0113d92
许若镔 <g.xurb@ffcs.cn>
2020-08-10 12:55:24 +0800

    首页

--- src/views/tab/home.vue
+++ src/views/tab/home.vue
@@ -47,19 +47,9 @@
               <div v-if="item.specificationNumber > 1" class="specification">
                 <van-button round type="info" size="mini" @click="selectSpecification(item.productId)">选规格</van-button>
               </div>
-<!--              规格弹出框-->
-              <van-sku
-                      v-model="specificationShow"
-                      :sku="sku"
-                      :goods="goods"
-                      :goods-id="goodsId"
-                      :quota="quota"
-                      :quota-used="quotaUsed"
-                      :hide-stock="sku.hide_stock"
-                      :message-config="messageConfig"
-                      @buy-clicked="onBuyClicked"
-                      @add-cart="onAddCartClicked"
-              />
+              <div>
+
+              </div>
             </template>
           </van-card>
         </van-list>
@@ -106,6 +96,7 @@
                 goods: {
                     // 数据结构见下方文档
                 },
+                goodsId:0,
                 messageConfig: {
                     // 数据结构见下方文档
                 },
@@ -197,36 +188,11 @@
                         }
                         this.loading = false
                     } else {
-                        console.log('wu')
                         this.list
                         this.dataList = []
                         this.finished = true
                     }
                 }
-                // this.dataList = [
-                //     {
-                //         productId:0,
-                //         productName:'名字',
-                //         minPrice:100,
-                //         productDesc:'描述',
-                //         // 购物车中的数量
-                //         productNumOfCart:0,
-                //         productStorage:10,
-                //         productPicturePath:'http://121.199.49.199:8082/upload/202008032221.jpg',
-                //         specificationNumber: 1
-                //     },
-                //     {
-                //         productId:1,
-                //         productName:'名字',
-                //         minPrice:100,
-                //         productDesc:'描述',
-                //         // 购物车中的数量
-                //         productNumOfCart:0,
-                //         productStorage:10,
-                //         productPicturePath:'http://121.199.49.199:8082/upload/202008032221.jpg',
-                //         specificationNumber: 2
-                //     }
-                // ]
             },
             // 获取分类
             async getProductCategoryList() {
@@ -250,170 +216,14 @@
             },
             // 根据id获取规格
             async getProductAttributeList(id) {
+                // 商品id
+                this.goodsId = id
                 // let { data } = await this.$api.getProductAttributeListByProductId(id)
                 // this.attributeList = data.data
                 // console.log('规格列表', this.attributeList)
-                this.sku = {
-                    // 所有sku规格类目与其值的从属关系，比如商品有颜色和尺码两大类规格，颜色下面又有红色和蓝色两个规格值。
-                    // 可以理解为一个商品可以有多个规格类目，一个规格类目下可以有多个规格值。
-                    tree: [
-                        {
-                            k: '颜色', // skuKeyName：规格类目名称
-                            k_s: 's1', // skuKeyStr：sku 组合列表（下方 list）中当前类目对应的 key 值，value 值会是从属于当前类目的一个规格值 id
-                            v: [
-                                {
-                                    id: '1', // skuValueId：规格值 id
-                                    name: '红色', // skuValueName：规格值名称
-                                    imgUrl: 'https://img.yzcdn.cn/1.jpg', // 规格类目图片，只有第一个规格类目可以定义图片
-                                    previewImgUrl: 'https://img.yzcdn.cn/1p.jpg', // 用于预览显示的规格类目图片
-                                },
-                                {
-                                    id: '1',
-                                    name: '蓝色',
-                                    imgUrl: 'https://img.yzcdn.cn/2.jpg',
-                                    previewImgUrl: 'https://img.yzcdn.cn/2p.jpg',
-                                }
-                            ],
-                            largeImageMode: true, //  是否展示大图模式
-                        }
-                    ],
-                        // 所有 sku 的组合列表，比如红色、M 码为一个 sku 组合，红色、S 码为另一个组合
-                        list: [
-                        {
-                            id: 2259, // skuId
-                            s1: '1', // 规格类目 k_s 为 s1 的对应规格值 id
-                            s2: '1', // 规格类目 k_s 为 s2 的对应规格值 id
-                            price: 100, // 价格（单位分）
-                            stock_num: 110 // 当前 sku 组合对应的库存
-                        }
-                    ],
-                        price: '1.00', // 默认价格（单位元）
-                        stock_num: 227, // 商品总库存
-                        collection_id: 2261, // 无规格商品 skuId 取 collection_id，否则取所选 sku 组合对应的 id
-                        none_sku: false, // 是否无规格商品
-                        messages: [
-                        {
-                            // 商品留言
-                            datetime: '0', // 留言类型为 time 时，是否含日期。'1' 表示包含
-                            multiple: '0', // 留言类型为 text 时，是否多行文本。'1' 表示多行
-                            name: '留言', // 留言名称
-                            type: 'text', // 留言类型，可选: id_no（身份证）, text, tel, date, time, email
-                            required: '1', // 是否必填 '1' 表示必填
-                            placeholder: '' // 可选值，占位文本
-                        }
-                    ],
-                        hide_stock: false // 是否隐藏剩余库存
-                }
             },
-            getSku() {
-                this.sku = {
-                    // 所有sku规格类目与其值的从属关系，比如商品有颜色和尺码两大类规格，颜色下面又有红色和蓝色两个规格值。
-                    // 可以理解为一个商品可以有多个规格类目，一个规格类目下可以有多个规格值。
-                    tree: [
-                        {
-                            k: '颜色',
-                            k_id: '1',
-                            v: [
-                                {
-                                    id: '30349',
-                                    name: '天蓝色',
-                                    imgUrl:
-                                        'https://img.yzcdn.cn/upload_files/2017/02/21/FjKTOxjVgnUuPmHJRdunvYky9OHP.jpg!100x100.jpg'
-                                },
-                                {
-                                    id: '1215',
-                                    name: '白色'
-                                }
-                            ],
-                            k_s: 's1',
-                            count: 2
-                        },
-                        {
-                            k: '尺寸',
-                            k_id: '2',
-                            v: [
-                                {
-                                    id: '1193',
-                                    name: '1'
-                                },
-                                {
-                                    id: '1194',
-                                    name: '2'
-                                }
-                            ],
-                            k_s: 's2',
-                            count: 2
-                        }
-                    ],
-                        // 所有 sku 的组合列表，如下是：白色1、白色2、天蓝色1、天蓝色2
-                        list: [
-                        {
-                            id: 2259,
-                            price: 120, //价格
-                            discount: 122,
-                            s1: '1215',
-                            s2: '1193',
-                            s3: '0',
-                            s4: '0',
-                            s5: '0',
-                            stock_num: 20, //库存
-                            goods_id: 946755
-                        },
-                        {
-                            id: 2260,
-                            price: 110,
-                            discount: 112,
-                            s1: '1215',
-                            s2: '1194',
-                            s3: '0',
-                            s4: '0',
-                            s5: '0',
-                            stock_num: 2, //库存
-                            goods_id: 946755
-                        },
-                        {
-                            id: 2257,
-                            price: 130,
-                            discount: 132,
-                            s1: '30349',
-                            s2: '1193',
-                            s3: '0',
-                            s4: '0',
-                            s5: '0',
-                            stock_num: 40, //库存
-                            goods_id: 946755
-                        },
-                        {
-                            id: 2258,
-                            price: 100,
-                            discount: 100,
-                            s1: '30349',
-                            s2: '1194',
-                            s3: '0',
-                            s4: '0',
-                            s5: '0',
-                            stock_num: 50, //库存
-                            goods_id: 946755
-                        }
-                    ],
-                        price: '5.00',
-                        stock_num: 227, // 商品总库存
-                        none_sku: false,  // 是否无规格商品
-                        hide_stock: false  // 是否隐藏剩余库存
-                }
-                // goods_id: '946755',
-                //     quota: 3, //限购数量
-                //     quota_used: 0,  //已经购买过的数量
-                //     goods_info: {
-                //     title: '测试商品A',
-                //         picture:
-                //     'https://img.yzcdn.cn/upload_files/2017/03/16/Fs_OMbSFPa183sBwvG_94llUYiLa.jpeg?imageView2/2/w/100/h/100/q/75/format/jpg'
-                // },
-                // initialSku:{
-                //     s1: "0001",
-                //         s2: "1001",
-                //         selectedNum: 3
-                // }
+            onAddCartClicked() {
+                console.log('加入购物车')
             }
         }
     }
@@ -428,5 +238,8 @@
     position: absolute;
     right: 20px;
     bottom: 8px;
+  }
+  /deep/.van-sku-row__title{
+    float: left;
   }
 </style>
